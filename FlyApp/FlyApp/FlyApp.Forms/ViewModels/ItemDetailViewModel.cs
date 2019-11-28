17de95071d606383525b45f343e37e9daf956cec
Vardan Sargsyan <vardan@xamlab.com>
2019-11-28 13:29:18 +0400

    initial commit

---
+++ FlyApp/FlyApp/FlyApp.Forms/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using FlyApp.Models;
+
+namespace FlyApp.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
